cd8dc8557b867a8740962c960bbaa65a1cca5115
Minisasy <Isabell.ljungberg2004@gmail.com>
2024-04-19 12:13:03 +0200

    dgfg

---
+++ TTI Pizza Game/Assets/Cutting meshes/SlliceObject.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+public class SlliceObject : MonoBehaviour
+{
+    public Transform startSlicePoint;
+    public Transform endSlicePoint;
+    public LayerMask sliceableLayer;
+    public VelocityEstimator velocityEstimator;
+
+    public Material crossSectionMaterial;
+    public float cutForce = 2000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+    void FixedUpdate()
+    {
+        bool hasHit = Physics.Linecast(startSlicePoint.position, endSlicePoint.position, out RaycastHit hit, sliceableLayer);
+
+        if (hasHit)
+        {
+            GameObject target = hit.transform.gameObject;
+            Slice(target);
+        }
+    }
+
+    public void Slice(GameObject target)
+    {
+        Vector3 velocity = velocityEstimator.GetVelocityEstimate();
+        Vector3 planeNormal = Vector3.Cross(endSlicePoint.position - startSlicePoint.position, velocity);
+        planeNormal.Normalize();
+
+        SlicedHull hull = target.Slice(endSlicePoint.position, planeNormal);
+
+        if (hull != null)
+        {
+            GameObject upperHull = hull.CreateUpperHull(target, crossSectionMaterial);
+            SetupSlicedComponent(upperHull);
+
+            GameObject lowerHull = hull.CreateLowerHull(target, crossSectionMaterial);
+            SetupSlicedComponent(lowerHull);
+
+            Destroy(target);
+        }
+    }
+
+    public void SetupSlicedComponent(GameObject slicedObject)
+    {
+        Rigidbody rb = slicedObject.AddComponent<Rigidbody>();
+        MeshCollider collider = slicedObject.AddComponent<MeshCollider>();
+        slicedObject.layer = LayerMask.NameToLayer("Sliceable");
+
+
+        collider.convex = true;
+        rb.AddExplosionForce(cutForce, slicedObject.transform.position, 1);
+    }
+}

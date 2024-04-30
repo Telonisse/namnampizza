c41147d46566edfaae6f3437e00e4f96c3a47b20
Minisasy <Isabell.ljungberg2004@gmail.com>
2024-04-30 13:37:34 +0200

    fridge start

--- TTI Pizza Game/Assets/Fridge/Fridge.cs
+++ TTI Pizza Game/Assets/Fridge/Fridge.cs
@@ -3,14 +3,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct toppingInFridge
+{
+    public string toppingTag;
+    public GameObject toppingGameObject;
+    public bool isInFridge;
+    public GameObject objectSpawnPos;
+}
+
 public class Fridge : MonoBehaviour
 {
+    [SerializeField] toppingInFridge[] toppingInFridgeArray;
     private HingeJoint joint;
     [SerializeField] bool open;
 
     private void Start()
     {
         joint = GetComponent<HingeJoint>();
+    }
+    private void Update()
+    {
+        bool isDone = false;
+        if (!open)
+        {
+            Collider[] colliders = Physics.OverlapBox(new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.5f), new Vector3(1, 2, 1), Quaternion.identity);
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                for (int j = 0; j < toppingInFridgeArray.Length; j++)
+                {
+                    if (colliders[i].tag == toppingInFridgeArray[j].toppingTag)
+                    {
+                        toppingInFridgeArray[j].isInFridge = true;
+                    }
+                }
+                isDone = true;
+            }
+        }
+        if (!open && isDone)
+        {
+            for (int i = 0; i < toppingInFridgeArray.Length; i++)
+            {
+                if (toppingInFridgeArray[i].isInFridge == false)
+                {
+                    Instantiate(toppingInFridgeArray[i].toppingGameObject, toppingInFridgeArray[i].objectSpawnPos.transform.TransformPoint(toppingInFridgeArray[i].objectSpawnPos.transform.localPosition), toppingInFridgeArray[i].objectSpawnPos.transform.rotation);
+                    toppingInFridgeArray[i].isInFridge = true;
+                }
+            }
+        }
     }
     public void OpenFridge()
     {
@@ -31,11 +71,25 @@ public class Fridge : MonoBehaviour
     {
         if (open)
         {
+            for(int i = 0;i < toppingInFridgeArray.Length; i++)
+            {
+                toppingInFridgeArray[i].isInFridge = false;
+            }
             open = false;
         }
         else
         {
             open = true;
         }
+    }
+    void OnDrawGizmos()
+    {
+        // Define the parameters for the overlap box
+        Vector3 center = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.5f);
+        Vector3 size = new Vector3(1, 2, 1);
+
+        // Draw the overlap box wireframe using Gizmos
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(center, size);
     }
 }

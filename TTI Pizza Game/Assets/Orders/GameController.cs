f3f751e5aee535a6c3d8f960d0ee651e7de34470
Minisasy <Isabell.ljungberg2004@gmail.com>
2024-05-14 12:04:45 +0200

    dfghjk

---
+++ TTI Pizza Game/Assets/Orders/GameController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] int[] pointLevel;
+
+    int currentLevel = 1;
+    private void Awake()
+    {
+        int numGameSessions = FindObjectsOfType<GameController>().Length;
+
+        if (numGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    private void Update()
+    {
+
+    }
+
+    public void Points(int points)
+    {
+        pointLevel[currentLevel - 1] += points;
+    }
+    public int CurrentPoints()
+    {
+        return pointLevel[currentLevel - 1];
+    }
+}
--- TTI Pizza Game/Assets/Orders/OrderHandler.cs
+++ TTI Pizza Game/Assets/Orders/OrderHandler.cs
@@ -1,25 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class OrderHandler : MonoBehaviour
 {
     [SerializeField] Order[] orders;
     [SerializeField] int numOfToppings;
+    [SerializeField] int pointsGoodPizza;
+    [SerializeField] int pointsBadPizza;
+    [SerializeField] TextMeshPro pointsText;
 
     private bool isOnPizza;
     private bool isOnOrder;
     private bool matchesOrder = false;
+    private GameController gameController;
+
+    private void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+        pointsText.text = gameController.CurrentPoints().ToString();
+        Debug.Log(gameController.CurrentPoints());
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other != null )
         {
-            PizzaScript pizza = other.GetComponentInChildren<PizzaScript>();
+            PizzaScript pizza = other.GetComponent<PizzaScript>();
             for (int i = 0; i < orders.Length; i++)
             {
                 matchesOrder = true;
-                if (orders[i].IsOffline() == false && other != null)
+                if (orders[i].IsOffline() == false && other != null && pizza != null)
                 {
                     for (int j = 0; j < numOfToppings; j++)
                     {
@@ -30,14 +42,22 @@ public class OrderHandler : MonoBehaviour
                             matchesOrder = false;
                         }
                     }
-                    if (matchesOrder && other != null)
+                    if (matchesOrder && other != null && pizza != null)
                     {
                         orders[i].ResetOrder();
                         Debug.Log(other.name);
-                        Destroy(other.transform.parent.gameObject);
+                        Destroy(other.transform.parent.parent.gameObject);
+                        gameController.Points(pointsGoodPizza);
+                        pointsText.text = gameController.CurrentPoints().ToString();
                         other = null;
                     }
                 }
+            }
+            if (!matchesOrder && pizza != null)
+            {
+                Debug.Log(other.name);
+                gameController.Points(pointsBadPizza);
+                pointsText.text = gameController.CurrentPoints().ToString();
             }
         }
     }

6745f71e8c57a91b6c50f2e779c7e4b596235344
Minisasy <Isabell.ljungberg2004@gmail.com>
2024-05-15 14:01:41 +0200

    sdfsdf

--- TTI Pizza Game/Assets/Spaces/FindSpawnPos.cs
+++ TTI Pizza Game/Assets/Spaces/FindSpawnPos.cs
@@ -215,6 +215,7 @@ public class FindSpawnPos : MonoBehaviour
                     }
                 }
             }
+            spawnedFridge.GetComponent<Collider>().enabled = false;
         }
     }
 
